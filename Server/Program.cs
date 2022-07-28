9215c06d85560f00434f6d65ac35ae1cbfe9c972
Jaedan <jaedan@none.com>
2022-07-28 15:51:51 -0700

    Initial commit with project layout

---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    ContentRootPath = Directory.GetCurrentDirectory()
+});
+
+Console.WriteLine($"ContentRoot Path: {builder.Environment.ContentRootPath}");
+Console.WriteLine($"WebRootPath: {builder.Environment.WebRootPath}");
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles(new StaticFileOptions
+{
+        ServeUnknownFileTypes = true,
+        DefaultContentType = "application/octet-stream"
+});
+
+app.Run();
