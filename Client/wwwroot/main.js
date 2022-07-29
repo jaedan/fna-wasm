ea4f677bfbec66106f6ed81f563f4b1b7d5297ae
3HMonkey <wewantthejoker@gmail.com>
2022-07-29 10:40:04 +0200

    Fixed load Module/canvas

--- Client/wwwroot/main.js
+++ Client/wwwroot/main.js
@@ -1,5 +1,6 @@
 import { App } from './app-support.js'
 
 App.init = async function () {
+    App.Module.canvas = document.getElementById('canvas');
     await App.MONO.mono_run_main("Client.dll", App.runArgs.applicationArguments);
 }
